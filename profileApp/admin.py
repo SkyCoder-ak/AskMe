d169e3a01ee49c4d0c1241df1ca916a1f6630fb0
SkyCoder <motheakash321@gmail.com>
2021-05-19 15:15:09 +0530

    created user model

--- profileApp/admin.py
+++ profileApp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import UserModel
 
 # Register your models here.
+admin.site.register(UserModel)
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('id','fullname','dis_name','email')
--- profileApp/models.py
+++ profileApp/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class UserModel(models.Model):
+    fullname = models.CharField(max_length=60)
+    dis_name = models.CharField(max_length=30)
+    designation = models.CharField(max_length=30)
+    city = models.CharField(max_length=30)
+    state = models.CharField(max_length=30)
+    email = models.EmailField(max_length=150)
+    password = models.CharField(max_length=16)
+
+    def __str__(self):
+        return self.fullname
+    
